019403163f0857db11b48f913507dad820e7fa81
Inilus <inilus@gmail.com>
2011-05-31 15:35:13 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylo::Application.config.secret_token = '78eec97734a7e35d61913fa33928c8e2f41287a072213d9f873e7cf6f7070aa908921e635dd0eb8fb7d9175e31e8e87184f651140ce859336ec4426b8bf83277'
