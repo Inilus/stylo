a8fa516b65b697ff3a6fe175c58a41c59aaf3b80
Inilus ZIO <inilus@gmail.com>
2011-06-01 15:21:19 +0400

    Add omniauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  before_filter :authenticate_user!, :except => [:index]
+
 end
+
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,13 @@
 
 Stylo::Application.config.session_store :cookie_store, key: '_stylo_session'
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Stylo::Application.config.session_store :active_record_store
+Stylo::Application.config.session = {
+  :key          => '_stylo_omniauth_session',   # name of cookie that stores the data
+  :domain       => nil,                         # you can share between subdomains here: '.communityguides.eu'
+  :expire_after => 1.day,                       # expire cookie
+  :secure       => false,                       # fore https if true
+  :httponly     => true,                        # a measure against XSS attacks, prevent client side scripts from accessing the cookie
+
+  :secret       => "07a0960800d8118a3bb51041fbe3a9b1ade2631548ca661c79ffddc3f293ffe80800cfb22fc8a9be0cb97c8f0c1e96e32980c5c061035d860c0640309f0a7f85"
+}
+
