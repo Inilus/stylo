dae8406bd082a10fbd73683f31ef834072a8f97a
Inilus ZIO <inilus@gmail.com>
2011-06-01 16:29:50 +0400

    Add mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,10 +11,24 @@ Stylo::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :test          # use this to NOT send emails in development mode
+
+  # Send emails via Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'stylo.dev@gmail.com',
+    :password             => 'funnybag',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
